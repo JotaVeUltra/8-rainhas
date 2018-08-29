eb52c0aa1cc28c94acf60a0d3618b2bfcfd5a2bf
João Marques <jotaveultra@gmail.com>
2018-08-29 20:12:02 -0300

    Update rainhas.py

--- rainhas.py
+++ rainhas.py
@@ -33,14 +33,14 @@ def mutar(filho):
     filho.insert(posição_2, gene)
 
 
-def gerar_individuo(n):
-    gene = list(range(n))
+def gerar_individuo():
+    gene = [1, 2, 3, 4, 5, 6, 7, 8]
     shuffle(gene)
     return gene
 
 
-def gerar_população(tamanho_população, n):
-    return [gerar_individuo(n) for _ in range(tamanho_população)]
+def gerar_população(tamanho_população):
+    return [gerar_individuo() for _ in range(tamanho_população)]
 
 
 def avaliar_adaptação(individuo):
@@ -58,18 +58,10 @@ def calcular_media(população):
     return sum(avaliar_adaptação(individuo) for individuo in população) / len(população)
 
 
-def executar_experimento(gerações, tamanho_população, taxa_mutação, elite, n):
-    população = gerar_população(tamanho_população, n)
-    # for geração in range(gerações):
-    geração = 0
-    while avaliar_adaptação(população[0]) > 0:
-        if geração%1000 == 0:
-            taxa_mutação += 1
-            print(f'media da população na geração {geração}:  {calcular_media(população)}')
-            print(f'taxa de mutação: {taxa_mutação}')
-        geração += 1
-        if geração%10_000 == 0:
-            break
+def executar_experimento(gerações, tamanho_população, taxa_mutação, elite):
+    população = gerar_população(tamanho_população)
+    for geração in range(gerações):
+        # print(f'media da população na geração {geração}:  {calcular_media(população)}')
         shuffle(população)
 
         nova_população = []
@@ -80,7 +72,7 @@ def executar_experimento(gerações, tamanho_população, taxa_mutação, elite,
             individuo_b = população.pop()
             nova_população.append(individuo_a)
             nova_população.append(individuo_b)
-            nova_população += cruzar(individuo_a, individuo_b, randint(2, len(individuo_a)-2), mutação)
+            nova_população += cruzar(individuo_a, individuo_b, randint(2, 6), mutação)
 
         população = sorted(nova_população, key=avaliar_adaptação)[:elite]
 
@@ -102,9 +94,9 @@ EXPERIMENTOS = [
 TAXA_MUTAÇÃO = 3  # %
 
 for gerações, tamanho_população, elite in EXPERIMENTOS:
-    resultados_experimento = executar_experimento(gerações, tamanho_população, TAXA_MUTAÇÃO, elite, 8)
+    resultados_experimento = executar_experimento(gerações, tamanho_população, TAXA_MUTAÇÃO, elite)
 
-    # print(f'gerações: {gerações}')
+    print(f'gerações: {gerações}')
     print(f'tamanho da população: {tamanho_população}')
     print(f'taxa de mutação: {TAXA_MUTAÇÃO}')
     print(f'média da população: {resultados_experimento[0]}')
